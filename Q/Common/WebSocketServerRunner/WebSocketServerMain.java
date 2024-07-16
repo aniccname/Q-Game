908361798216017b978754beddd957797c7a71d0
Nicholas <freeman.n@northeastern.edu>
2024-07-16 17:37:43 -0400

    Add a help option to server executable.

--- Q/Common/WebSocketServerRunner/WebSocketServerMain.java
+++ Q/Common/WebSocketServerRunner/WebSocketServerMain.java
@@ -12,6 +12,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -29,6 +30,9 @@ import Referee.Referee;
  * Input read in the format of port rounds timeout name-timeout
  */
 public class WebSocketServerMain {
+  private static String getStoreType(String path) {
+    return path.substring(path.lastIndexOf(".") + 1).toUpperCase();
+  }
 
   private static Optional<KeyStore> initializeKeyStore(String storetype, String storepass, String storepath) {
     KeyStore ks = null;
@@ -102,39 +106,64 @@ public class WebSocketServerMain {
     }
     return initializeSSLContext(kmf.get(), tmf.get());
   }
-  public static void main(String[] args) throws IOException {
-    if (args.length != 4) {
-      System.out.println("Invalid number of arguments. Please supply the port number, " +
-              "number of waiting periods, length of each waiting period, and turn timeout in seconds.");
-      return;
-    }
 
-    int port = Integer.parseInt(args[0]);
-    int rounds = Integer.parseInt(args[1]);
-    int roundLength = Integer.parseInt(args[2]);
-    int playerTimeout = Integer.parseInt(args[3]);
+  /**
+   * Runs a server with the given options. Prompts the user for a keystore and password through system.in.
+   * @param port the port to host the server on.
+   * @param rounds the number of waiting periods this server will try before ending the connections.
+   * @param roundLength the length of each waiting period.
+   * @param playerTimeout the maximum time allotted for each player to take a turn.
+   */
+  private static void runServer(int port, int rounds, int roundLength, int playerTimeout) {
     int NAME_TIMEOUT = 3;
-
     var refConfig = new RefereeConfig.RefereeConfigBuilder().playerTimeoutInSeconds(playerTimeout).build();
     var config = new ServerConfig.ServerConfigBuilder().port(port).quiet(false).waitingPeriodLengthInSeconds(roundLength)
             .numWaitingPeriods(rounds).waitForNameInSeconds(NAME_TIMEOUT).refereeConfig(refConfig).build();
 
-    QGameWebSocketServer server = new QGameWebSocketServer(new Referee(), config, port,"localhost");
+    QGameWebSocketServer server = new QGameWebSocketServer(new Referee(), config, port,"192.168.1.162");
     Scanner scanner = new Scanner(new InputStreamReader(System.in));
     System.out.println("Please input the path to the keystore:");
     String STOREPATH = scanner.nextLine();
-    System.out.println("Please input the type of the keystore:");
-    String STORETYPE = scanner.nextLine();
-    System.out.println("Please enter the password to the keystore:");
+    System.out.println("Please input the password of the keystore:");
+    String STORETYPE = getStoreType(STOREPATH);
     String STOREPASS = scanner.nextLine();
 
     getSSLContext(STORETYPE, STOREPASS, STOREPATH)
             .ifPresentOrElse((ssl) -> {
-              System.out.println("Certificate successfully loaded. Starting server with wss support");
-              server.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(ssl));},
+                      System.out.println("Certificate successfully loaded. Starting server with wss support");
+                      server.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(ssl));},
                     () -> System.out.println("Unable to load certificate and initialize SSL context." +
                             " Starting server without certificate validation."));
 
     server.run();
+  }
+  private static boolean isHelpArg(String arg) {
+    return "-h".equalsIgnoreCase(arg)
+            || "-help".equalsIgnoreCase(arg);
+  }
+  private static void printHelp() {
+    System.out.println("usage: server [port] [num-rounds] [round-length] [max-turn-length]");
+    System.out.println("port is the port the server will be hosted on.");
+    System.out.println("num-rounds is the number of waiting periods the server will try before " +
+            "ending the game.");
+    System.out.println("If at the end of a round at least 2 players are present, the game "+
+            "will start. If not, a new round will commence if it's not the last round");
+    System.out.println("If it is the last round, the server will report the end of the game. ");
+    System.out.println("round-length is the length of each round in seconds.");
+    System.out.println("max-turn-length is the maximum length allotted for a player's turn in " +
+            "seconds.");
+    System.out.println("A player will be disconnected from the game if they do not finish their " +
+            "turn within this allotted time. ");
+  }
+  public static void main(String[] args) throws IOException {
+    if (Arrays.stream(args).anyMatch(WebSocketServerMain::isHelpArg)) {
+      printHelp();
+      return;
+    }
+    if (args.length != 4) {
+      System.out.println("Invalid number of arguments. For instructions please use the -h argument.");
+      return;
+    }
+    runServer(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
   }
 }
